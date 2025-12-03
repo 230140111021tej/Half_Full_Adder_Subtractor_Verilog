73c0b8d55d9cdbe37bce1c6fc16310c2d730ce37
Mallah Tejas Rajkumar <230140111020@gtu.edu.in>
2025-12-03 22:00:56 +0530

    Add Verilog implementation of a full adder

---
+++ FULL_Adder.v
@@ -0,0 +1,5 @@
+// Full Adder in Verilog
+module full_adder(input a, input b, input cin, output sum, output carry);
+    assign sum = a ^ b ^ cin;
+    assign carry = (a & b) | (b & cin) | (cin & a);
+endmodule
